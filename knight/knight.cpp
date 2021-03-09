98e9284891828db5099a5bb44f7a0fc1902d2700
Ivan Kolesnev <i.v.kolesnev@mail.ru>
2021-03-09 22:46:59 +0300

    implement structure for knight

---
+++ knight/knight.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int xCoordForWhiteKnight = 0, yCoordForWhiteKnight = 0, xCoordForBlackKnight = 0, yCoordForBlackKnight = 0;
+
+    cout << "Please, write x coordinate for white knight: ";
+    cin >> xCoordForWhiteKnight;
+    cout << "Please, write y coordinate for white knight: ";
+    cin >> yCoordForWhiteKnight;
+    cout << "Please, write x coordinate for black knight: ";
+    cin >> xCoordForBlackKnight;
+    cout << "Please, write y coordinate for black knight: ";
+    cin >> yCoordForBlackKnight;
+
+    if (abs(xCoordForWhiteKnight - xCoordForBlackKnight) * abs(yCoordForWhiteKnight - yCoordForBlackKnight) == 2)
+    {
+        cout << "Kick knight" << endl;
+    }
+    else
+    {
+        cout << "No kick knight" << endl;
+    }
+}
