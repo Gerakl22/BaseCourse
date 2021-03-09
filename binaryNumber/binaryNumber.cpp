3029cbed64a5567ae7ddc9686764bf68c36ae10a
Ivan Kolesnev <i.v.kolesnev@mail.ru>
2021-03-09 22:59:08 +0300

    implement structure for binaryNumber

---
+++ binaryNumber/binaryNumber.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int sum = 0, number = 0, binaryNumber = 0, counter = 0;
+
+    cout << "Please, write binary Number: ";
+    cin >> binaryNumber;
+
+    while (binaryNumber > 0)
+    {
+        number = binaryNumber % 10;
+        if (number == 1)
+        {
+            sum += pow(2, counter);
+        }
+
+        counter += 1;
+        binaryNumber /= 10;
+    }
+
+    cout << "Sum: " << sum << endl;
+}
