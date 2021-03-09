1e20f327d9487b7c1088b0888f7266c7596e3cf0
Ivan Kolesnev <i.v.kolesnev@mail.ru>
2021-03-09 22:14:33 +0300

    implement structure for mutants

---
+++ mutants/mutants.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double numberOfMutants = 0, percentMultiplicationOfMutants = 0;
+    int killMutants = 0, daysForVictoryOverMutants = 0;
+
+    cout << "Please, write number of mutants: ";
+    cin >> numberOfMutants;
+    cout << "Please, write how much percent of increase mutants: ";
+    cin >> percentMultiplicationOfMutants;
+    cout << "Please, write how much mutants need to kill: ";
+    cin >> killMutants;
+
+    while (numberOfMutants > 0)
+    {
+
+        numberOfMutants = (((numberOfMutants * percentMultiplicationOfMutants) / 100) + numberOfMutants) - killMutants;
+        daysForVictoryOverMutants++;
+    }
+
+    cout << "Days for victory over mutants: " << daysForVictoryOverMutants << endl;
+}
