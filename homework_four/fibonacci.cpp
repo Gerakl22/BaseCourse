0cf6e6de10049fa135ad76abb43ad741587054f0
Ivan Kolesnev <i.v.kolesnev@mail.ru>
2021-03-14 20:20:07 +0300

    func fibonacci with recursia

---
+++ homework_four/fibonacci.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int fibonacci(int number)
+{
+    if (number < 1)
+    {
+        return 0;
+    }
+
+    if (number == 1 || number == 2)
+    {
+        return 1;
+    }
+
+    return fibonacci(number - 1) + fibonacci(number - 2);
+}
+
+int main()
+{
+    int number = 0;
+
+    cout << "Write number: ";
+    cin >> number;
+
+    for (int i = 0; i <= number; i++)
+    {
+        cout << fibonacci(i) << endl;
+    }
+
+    return 0;
+}
