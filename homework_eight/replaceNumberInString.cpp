3214fa7b580a31af2fbe621699c102c5b1f5f2f0
Ivan Kolesnev <i.v.kolesnev@mail.ru>
2021-04-11 22:18:11 +0300

    implement: func replaceNumberInString

---
+++ homework_eight/replaceNumberInString.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+#define NUMBER 128
+#define PLUS '+'
+#define MINUS '-'
+
+void replaceNumberInString(char string[]);
+
+int main()
+{
+    char string[NUMBER] = "3f5a4f3ssd1";
+
+    replaceNumberInString(string);
+
+    return 0;
+}
+
+void replaceNumberInString(char string[])
+{
+    char replaceArr[NUMBER];
+    int number = 0, index = 0;
+
+    for (int i = 0; string[i] != '\0'; i++)
+    {
+        if (string[i] >= '0' && string[i] <= '9')
+        {
+            number = string[i] - '0';
+
+            if (number % 2 == 0)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    replaceArr[index++] = PLUS;
+                }
+            }
+            else
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    replaceArr[index++] = MINUS;
+                }
+            }
+        }
+        else
+        {
+            replaceArr[index++] = string[i];
+        }
+    }
+    replaceArr[index] = '\0';
+
+    cout << replaceArr;
+}
